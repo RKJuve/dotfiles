d1d15c2fdc14637ddd30d6af8e814adf6df7404b
Ryan Juve <rkjuve@gmail.com>
2022-05-04 11:43:29 -0700

    try the .bashrc approach

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-alias gs="git status"
-alias gdh="git diff HEAD"
-alias gdhs="git diff HEAD --stat"
-alias gpm="git pull origin master"
-alias rebase-latest-master="git co master && gpm && git co - && git rebase master"
+# append aliases to the .bashrc, because VSCode doesn't run .bash_profile (https://github.com/microsoft/vscode/issues/20265#issuecomment-278818467)
+echo "alias gs=\"git status\"" >> ~/.bashrc
+echo "alias gdh=\"git diff HEAD\"" >> ~/.bashrc
+echo "alias gdhs=\"git diff HEAD --stat\"" >> ~/.bashrc
+echo "alias gpm=\"git pull origin master\"" >> ~/.bashrc
+echo "alias rebase-latest-master=\"git co master && gpm && git co - && git rebase master\"" >> ~/.bashrc
